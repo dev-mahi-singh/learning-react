7e2163c10a43072d35cd2e3dd85e394dc4e99f3d
Mahi Singh <singh.dev.mahi@gmail.com>
2026-02-18 22:56:47 +0530

    use of fragments

--- 01-folder/src/App.jsx
+++ 01-folder/src/App.jsx
@@ -1,5 +1,15 @@
-function App(){
-  return 'Hello'
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className="dad">
+      <h1 className="child1">Hello</h1>
+      <h2 className="child2">Hello</h2>
+    </div>
+    <div className="chacha"></div>
+    </>
+  )
 }
 
 export default App
