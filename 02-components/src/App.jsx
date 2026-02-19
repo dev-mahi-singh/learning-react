2ad87b3c59a7f1d454b6bb937b04e546e38834fd
Mahi Singh <singh.dev.mahi@gmail.com>
2026-02-19 19:10:23 +0530

    print variable

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+
+  const user = "Mahie Singh";
+
+  return (
+    <div>
+      <div className="card">
+        <h1>{user}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, consectetur.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
+App
