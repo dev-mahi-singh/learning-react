33c73549ca5bc900275de6eee04f1f8be11ad4c7
Mahi Singh <singh.dev.mahi@gmail.com>
2026-02-21 15:30:07 +0530

    cards ui done

---
+++ 03-props/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+     <div className="card">
+        <img src="https://images.unsplash.com/photo-1751097006268-ae4601fcb117?q=80&w=1059&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <h1>Mahie Singh</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, odit!</p>
+        <button>View Profile</button>
+      </div>
+  )
+}
+
+export default Card
