e63d60c8f4354997a36172c50bb20a7908049c42
Mahi Singh <singh.dev.mahi@gmail.com>
2026-02-21 13:41:17 +0530

    components done

--- 02-components/src/App.jsx
+++ 02-components/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
 
 const App = () => {
 
-  const user = "Mahie Singh";
-
   return (
     <div>
-      <div className="card">
-        <h1>{user}</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, consectetur.</p>
-      </div>
+      <Navbar />
+      <Navbar />
+      <Card />
+      <Card />
+      <Card />
     </div>
   )
 }
 
 export default App
-App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className="card">
+            <h1>Mahie Singh</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, consectetur.</p>
+        </div>
+    )
+}
+
+export default Card
