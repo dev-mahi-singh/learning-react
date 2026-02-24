29a5ae770decb5536841424f3ac61f50dad7420b
Mahi Singh <singh.dev.mahi@gmail.com>
2026-02-24 21:35:42 +0530

    tailwind setup

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900 flex items-center justify-center'>
+      <h1 className='text-6xl font-bold text-white'>Sheryians Coding School</h1>
+    </div>
+  )
+}
+
+export default App
