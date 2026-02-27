432a97ddb613d3b203d701bac8053f26a0e3d5d7
Mahi Singh <singh.dev.mahi@gmail.com>
2026-02-27 14:20:11 +0530

    access object's value

--- 10-usestate-advance/src/App.jsx
+++ 10-usestate-advance/src/App.jsx
@@ -2,17 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
 
-  const [num, setNum] = useState(10)
+  const [num, setNum] = useState({ user: 'Mahie', age: 19 })
 
-  const btnClicked=()=>{
-    console.log(num);
-    setNum(20)
-    console.log(num);
+  const btnClicked = () => {
+    console.log(num.user);
+    console.log(num.age);
   }
 
   return (
     <div className='parent'>
-      <h1>{num}</h1>
+      <h1>{num.user}, {num.age}</h1>
       <button onClick={btnClicked}>Click</button>
     </div>
   )
