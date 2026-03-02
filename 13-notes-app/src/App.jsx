0bbac89a0296e98eddac16cac3f2b87d2a3c6040
Mahi Singh <singh.dev.mahi@gmail.com>
2026-03-02 09:57:16 +0530

    ui done

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -5,20 +5,28 @@ const App = () => {
   const submitHandler = (e) => {
     e.preventDefault()
     console.log("form submitted");
-    
+
   }
 
   return (
-    <div className='h-screen bg-black text-white'>
+    <div className='h-screen lg:flex bg-black text-white'>
       <form onSubmit={(e) => {
         submitHandler(e)
-      }} className="flex justify-between items-start p-10">
-        <div className="flex gap-4 w-1/2 items-start flex-col">
-          <input type="text" placeholder='Enter Notes Heading' className='px-5 w-full py-2 border-2 rounded outline-none font-medium' />
-          <textarea placeholder='Write Details' className='px-5 w-full h-32 py-2 flex items-start flex-row border-2 font-medium rounded outline-none'></textarea>
-          <button className='bg-white w-full  text-black px-5 py-2 rounded outline-none font-medium'>Add Note</button>
-        </div>
+      }} className="flex lg:w-1/2 items-start gap-4 flex-col p-10">
+        <h1 className='text-4xl font-bold'>Add Notes</h1>
+
+        <input type="text" placeholder='Enter Notes Heading' className='px-5 w-full py-2 border-2 rounded outline-none font-medium' />
+        <textarea placeholder='Write Details' className='px-5 w-full h-32 py-2 flex items-start flex-row border-2 font-medium rounded outline-none'></textarea>
+        <button className='bg-white w-full text-black px-5 py-2 rounded outline-none font-medium'>Add Note</button>
       </form>
+      <div className="lg:w-1/2 lg:border-l-2 p-10">
+        <h1 className='text-4xl font-bold'>Recent Notes</h1>
+        <div className='flex flex-wrap gap-5 mt-5 h-full overflow-auto'>
+          <div className="h-52 w-40 rounded-2xl bg-white"></div>
+          <div className="h-52 w-40 rounded-2xl bg-white"></div>
+          <div className="h-52 w-40 rounded-2xl bg-white"></div>
+        </div>
+      </div>
     </div>
   )
 }
