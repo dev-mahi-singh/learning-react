6216f7fa5b8b57c957cce8da2fd19f10233b85eb
Mahi Singh <singh.dev.mahi@gmail.com>
2026-03-17 11:28:10 +0530

    Update UI to show fetched images dynamically using axios

--- 15-api-calling/src/App.jsx
+++ 15-api-calling/src/App.jsx
@@ -1,17 +1,29 @@
 // install axios using command "npm i axios"
 import axios from 'axios';
+import { useState } from 'react';
 
 const App = () => {
 
+  const [data, setData] = useState([]);
+
   const getData = async () => {
-    const {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
-    console.log(data);
-    
+    const response = await axios.get('https://picsum.photos/v2/list');
+    setData(response.data);
+
+
   }
 
   return (
     <div>
       <button onClick={getData}>Get Data</button>
+      <div className='parent'>
+        {data.map(function (elem, idx) {
+          return <div key={idx} className='card'>
+            <h2>{elem.author}</h2>
+            <img src={elem.download_url} alt="" />
+          </div>
+        })}
+      </div>
     </div>
   )
 }
