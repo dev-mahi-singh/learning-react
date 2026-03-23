d7913538750e2968b58ed016ef830e3101dbc4a5
Mahi Singh <singh.dev.mahi@gmail.com>
2026-03-23 23:17:39 +0530

    Add independent state handling with multiple useEffect hooks

--- 16-useeffect/src/App.jsx
+++ 16-useeffect/src/App.jsx
@@ -1,24 +1,35 @@
 import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [num, setNum] = useState(0);
-  const [num2, setNum2] = useState(100);
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
 
-  useEffect(function () {
-    console.log("use effect is running...");
-  }, [num]);
+  const aChanging = () => {
+    console.log("Value of A was changed");
+  };
+
+  const bChanging = () => {
+    console.log("Value of B was changed");
+  };
+
+  useEffect(() => {
+    aChanging();
+  }, [a]);
+
+  useEffect(() => {
+    bChanging();
+  }, [b]);
 
   return (
     <div>
-      <h1>Num {num}</h1>
-      <h1>Num2 {num2}</h1>
-      <button onMouseEnter={() => {
-        setNum(num + 1)
-      }}
-        onMouseLeave={() => {
-          setNum2(num2 + 10)
-        }}
-      >Hover</button>
+      <h1>A is {a}</h1>
+      <h1>B is {b}</h1>
+      <button onClick={() => {
+        setA(a + 1)
+      }}>Change A</button>
+      <button onClick={() => {
+        setB(b + 1)
+      }}>Change B</button>
     </div>
   )
 }
