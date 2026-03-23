4da75a2dce8fea5c68113b9269a864a187b90792
Mahi Singh <singh.dev.mahi@gmail.com>
2026-03-23 22:53:21 +0530

    Add useEffect with num dependency

--- 16-useeffect/src/App.jsx
+++ 16-useeffect/src/App.jsx
@@ -2,17 +2,23 @@ import React, { useEffect, useState } from 'react'
 
 const App = () => {
   const [num, setNum] = useState(0);
+  const [num2, setNum2] = useState(100);
 
   useEffect(function () {
     console.log("use effect is running...");
-  }, []);
+  }, [num]);
 
   return (
     <div>
-      <h1>{num}</h1>
-      <button onClick={() => {
+      <h1>Num {num}</h1>
+      <h1>Num2 {num2}</h1>
+      <button onMouseEnter={() => {
         setNum(num + 1)
-      }}>Click</button>
+      }}
+        onMouseLeave={() => {
+          setNum2(num2 + 10)
+        }}
+      >Hover</button>
     </div>
   )
 }
